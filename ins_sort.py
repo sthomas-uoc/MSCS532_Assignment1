af8d76ce0f307d0be057097fa2ef68b0f3913259
Sonie Thomas <sthomas44707@ucumberlands.edu>
2025-10-26 19:24:24 -0400

    Add comments, random data test

--- ins_sort.py
+++ ins_sort.py
@@ -1,10 +1,12 @@
+import random
 
+# Sorts data using insertion sort in monotoncally decreasing order
 def ins_sort(data):
     for i in range(0, len(data) - 1):
         # print(i)
         for j in range(i, len(data)):
             # print(i, j)
-            if data[i] < data[j]:
+            if data[i] < data[j]: # Ensures decreasing order
                 t = data[i]
                 data[i] = data[j]
                 data[j] = t
@@ -27,6 +29,7 @@ def demo_ins_sort():
     assert 0 == data[len(data) - 1]
     assert 9 == data[0]
 
+    # Create reversed serial data
     data = list(range(10, 0, -1))
     # print(data)
     ins_sort(data)
@@ -34,7 +37,12 @@ def demo_ins_sort():
     assert 1 == data[len(data) - 1]
     assert 10 == data[0]
 
-    
+    # Create random data
+    data = random.sample(range(50, 5000), 100)
+    # print(data)
+    ins_sort(data)
+    # print(data)
+    assert data[0] > data[len(data) - 1]
 
 if __name__ == "__main__":
     demo_ins_sort()
